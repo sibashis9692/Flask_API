66c4aaed58a19df1a3ab6df41997776eaefc8ac8
sibashis Nayak <sibashisnayak228@gmail.com>
2023-08-05 23:35:48 +0530

    Use flask_restfu, improve error handling and updated screenshots

--- main.py
+++ main.py
@@ -1,90 +1,84 @@
 from bson import ObjectId
 from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
 from pymongo import MongoClient
+from marshmallow import Schema, fields, ValidationError
 
 app = Flask("__name__")
+api = Api(app)
 
 client = MongoClient('localhost', 27017)
-
 db = client.User
 collections = db.User_data
 
+# This class is help to ensure required fields are given by the user
+class requreFields(Schema):
+    name = fields.String(required=True)
+    email = fields.String(required=True)
+    password = fields.String(required=True)
 
-# Get or Create Users data
-@app.route("/users", methods=['GET','POST'])
-def getUsers():
-    if(request.method == 'GET'):
+# Here we make an instance of class requreFields
+fields = requreFields()
+
+
+class UsersResource(Resource):
+    # Get all users
+    def get(self):
         users = list(collections.find())
         for user in users:
             user['_id'] = str(user['_id'])
-        if(len(users) == 0):
-            return jsonify(
-                {
-                    "message":"No users are available."
-                }
-            )
-        return jsonify(users)
-    
-    elif(request.method == 'POST'):
-        fields={'name':False, 'email':False, 'password':False}
-        requreFields = ""
-        data = request.get_json()
-        for field in fields.keys():
-            if(data.__contains__(field)):
-                fields[field] = True
-            else:
-                requreFields += f"{field}, "
-        if(requreFields != ""):
-            return jsonify(
-                {
-                    "message": f"{requreFields} fields are required."
-                }
-            )
-        collections.insert_one(data)
-        return jsonify(
-            {
-                "message": "Successfully added."
-            }
-        )
-    
-# Get, Update, Delete Users data
-@app.route("/users/<string:id>", methods=['GET', 'PUT', 'DELETE'])
-def getUser(id):
 
-    # Here we check the id is valid or not
-    if(not ObjectId.is_valid(id)):
-        return jsonify(
-        {
-            "message": "Invalid ID"
-        }
-    )
+        if not users:
+            return {"message": "No users are available."}, 404
 
-    # Here we check if the id is exist or not
-    elif(not collections.find_one({'_id': ObjectId(id)})):
-            return jsonify({'message': 'User not found'})
-    
-    elif(request.method == 'GET'):
-        user = collections.find_one({'_id': ObjectId(id)})
-        user['_id'] = str(user['_id'])
-        return jsonify(user)
+        return users, 200
+
+    # Add new user
+    def post(self):
+        try:
+            data = fields.load(request.get_json())
+        except ValidationError as e:
+            error = e.normalized_messages()
+            return {"errors": error}, 400
         
-    elif(request.method == 'PUT'):
+        collections.insert_one(data)
+        return {"message": "Successfully added"}, 201
+
+
+class UserResource(Resource):
+
+    # Get Specific single user
+    def get(self, id):
+        if not ObjectId.is_valid(id):
+            return {"message": "Invalid ID"}, 400
+
+        user = collections.find_one({'_id': ObjectId(id)})
+        if not user:
+            return {"message": "User not found"}, 404
+
+        user['_id'] = str(user['_id'])
+        return user, 200
+
+    # Update the specific user
+    def put(self, id):
+        if not ObjectId.is_valid(id):
+            return {"message": "Invalid ID"}, 400
+
         data = request.get_json()
         collections.update_one({'_id': ObjectId(id)}, {'$set': data})
-        return jsonify(
-            {
-                "message": "Sucessfully Updated"
-            }
-        )
-    
-    elif(request.method == 'DELETE'):
-        user = collections.delete_one({'_id': ObjectId(id)})
-        return jsonify(
-            {
-                "message": "Successfully deleted."
-            }
-        )
-    
+        return {"message": "Successfully updated"}, 200
+
+    # Delete Specific user
+    def delete(self, id):
+        if not ObjectId.is_valid(id):
+            return {"message": "Invalid ID"}, 400
+        collections.delete_one({'_id': ObjectId(id)})
+        return {"message": "Successfully deleted"}, 200
+
+# Endpoints
+api.add_resource(UsersResource, '/users')
+api.add_resource(UserResource, '/users/<string:id>')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
